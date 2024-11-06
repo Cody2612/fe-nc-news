304316b0ed5d878f8dc03459d1eafd41405948ce
Cody2612 <b.cody_26@yahoo.com>
2024-11-06 21:46:58 +0000

    props drilled the comments and added an empty [] to not error if user gets no comments

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -2,14 +2,20 @@ import React from 'react'
 import CommentCard from './CommentCard';
 
 
-const Comments = ({comments}) => {
+const Comments = ({comments = [], currentUser, onDeleteComment, article_id}) => {
 
 return (
   <>
     <h2>Comments</h2>
     <div>
         {comments.length > 0 ? (comments.map((comment) => (
-          <CommentCard key={comment.comment_id}  {...comment}/>
+          <CommentCard 
+            key={comment.comment_id}  
+            {...comment}
+            currentUser={currentUser}
+            onDeleteComment={onDeleteComment}
+            article_id={article_id}
+            />
         ))):(
           <p>No comments</p>
         )}
