30465cdfbdc7a8f1f32734733c60717bd8785c54
Cody2612 <b.cody_26@yahoo.com>
2024-11-06 10:38:25 +0000

    add the Patch article votes api call with the current state

--- src/api.js
+++ src/api.js
@@ -5,28 +5,33 @@ const api = axios.create({
     baseURL: "https://myfirstapi-vhbe.onrender.com/api"
 })  
 
-const getArticles = () => {
+export const getArticles = () => {
     return api.get("/articles").then((response)=>{
         return response.data.articles;
     })
 }
 
-const getTopics = () => {
+export const getTopics = () => {
     return api.get("/topics").then((response)=>{
         return response.data.topics;
     })
 }
 
-const getArticlesById = (id) => {
+export const getArticlesById = (id) => {
     return api.get(`/articles/${id}`).then((response)=>{
         return response.data.article;
     })
 }
 
-const getCommentsByArticleId = (id) => {
+export const getCommentsByArticleId = (id) => {
     return api.get(`/articles/${id}/comments`).then((response)=>{
         return response.data.comments;
     })
 }
 
-export { getArticles, getTopics, getArticlesById, getCommentsByArticleId };
+
+export const patchArticleVotes = (id, updatedVote) => {
+    return api.patch(`/articles/${id}`, {inc_votes: updatedVote}).then((response)=>{
+        return response.data.article;
+    })
+}
