e6b224277b410e0b91d29a7767cf5dfd327992a5
Cody2612 <b.cody_26@yahoo.com>
2024-11-06 20:04:50 +0000

    changed the props to comments and updated the child component

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,44 +1,15 @@
-import React, { useEffect, useState } from 'react'
-import { getCommentsByArticleId } from '../api';
-import ErrorPage from './ErrorPage';
-import SingleComment from './SingleComment';
+import React from 'react'
+import CommentCard from './CommentCard';
 
 
-const Comments = ({article_id}) => {
-  const [comments, setComments] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-
-
-useEffect(()=>{
-  setIsLoading(true);
-   getCommentsByArticleId(article_id)
-   .then((commentsData) =>{
-    setComments(commentsData);
-       setIsLoading(false);
-    })
-    .catch((error)=>{
-        setIsLoading(false)
-        setIsError(`Error loading comments`)
-        console.log("No comments", error);
-   });
-}, [article_id]);
-
-if (isLoading){
-    return <p>Loading...</p>;
-}
-
-if(isError){
-   return <ErrorPage />;
-}
-
+const Comments = ({comments}) => {
 
 return (
   <>
     <h2>Comments</h2>
     <div>
         {comments.length > 0 ? (comments.map((comment) => (
-          <SingleComment key={comment.comment_id}  {...comment}/>
+          <CommentCard key={comment.comment_id}  {...comment}/>
         ))):(
           <p>No comments</p>
         )}
